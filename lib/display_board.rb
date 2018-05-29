d334f5b60c76aedf6b542e8809682ae5cb42186f
Christofer Chan <devchumbing@gmail.com>
2018-05-29 22:48:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(mark)
-  "   |   |   "
+  " #{mark[0]}  |   |   "
   "-----------"
   "   |   |   "
   "-----------"
