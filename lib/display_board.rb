808282081180baededbdea323bd361de237f21f9
Christofer Chan <devchumbing@gmail.com>
2018-05-29 22:48:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["",]
-def display_board(args)
-  "#{args[0]}   | #{args[1]} | #{args[2]}  \n-----------\n  #{args[3]} | #{args[4]}  | #{args[5]} \n-----------\n  #{args[6]} | #{args[7]}  | #{args[8]}""
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(mark)
+  "   |   |   "
+  "-----------"
+  "   |   |   "
+  "-----------"
+  "   |   |   "
+end
